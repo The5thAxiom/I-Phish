4487620f321645b1bb7fe93e1d5fc86769b53597
The5thAxiom <samridh.anand.paatni@gmail.com>
2022-12-11 11:21:02 +0530

    run_model will check for type

--- model/__init__.py
+++ model/__init__.py
@@ -1,2 +1,7 @@
-def run_model(url: str) -> str:
-    return 'ok'
+import random
+
+def run_model(url: str) -> bool:
+    if type (str) is str:
+        return True if random.randint(0, 1) else False
+    else:
+        raise TypeError("url should be a string")
