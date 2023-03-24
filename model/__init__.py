299e7649dd05c6e9f9e09ffebeaee1df593880f0
The5thAxiom <samridh.anand.paatni@gmail.com>
2023-03-24 18:11:55 +0530

    added comments to the model

--- model/__init__.py
+++ model/__init__.py
@@ -9,14 +9,22 @@ def resolve_url(url: str) -> str:
 
 def predict(url: str) -> bool:
     if type(url) is str:
+        # resolving the url:
+        # this will get us the actual URL and not just the redirect
         try:
             final_url = resolve_url(url)
         except:
             return False, "could not resolve URL"
+        
+        # feature engineering:
+        # here, we convert the raw URL into a DataFrame, containing the features we need
         try:
             urlData = feature_engineer(final_url)
         except:
             return False, "feature engineering failed"
+        
+        # making predictions:
+        # now, we give the features we got above into the model to get our predictions
         return True if random.randint(0, 1) else False, "OK"
     else:
         raise TypeError("url should be a string")
