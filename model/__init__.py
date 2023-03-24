b58fa7b238d6936262ea8e70bcd8f00047c7247a
The5thAxiom <samridh.anand.paatni@gmail.com>
2023-03-24 17:00:51 +0530

    model random output error corrected

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,7 @@
 import random
 
 def run_model(url: str) -> bool:
-    if type (str) is str:
+    if type(url) is str:
         return True if random.randint(0, 1) else False
     else:
         raise TypeError("url should be a string")
