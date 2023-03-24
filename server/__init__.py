50c867763d06b7898d47f166a326667872dee7ef
The5thAxiom <samridh.anand.paatni@gmail.com>
2023-03-24 17:00:33 +0530

    added cors to server

--- server/__init__.py
+++ server/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from model import run_model
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/is-valid-url', methods=['POST'])
 def validate():
