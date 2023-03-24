d51e79a78e18c1e90e6e08c49e73b05d1d4c7b5b
The5thAxiom <samridh.anand.paatni@gmail.com>
2023-03-24 15:33:12 +0530

    implemented basic extension (random output)

---
+++ extension/checkURL.js
@@ -0,0 +1,49 @@
+const tags = tag => Array.from(document.getElementsByTagName(tag));
+
+/*
+type Link {
+    tag: HtmlELement;
+    href: string;
+    hasError: boolean;
+    isValid: boolean;
+};
+*/
+
+async function validateURLS(links) {
+    // links: Link[]
+
+    // sleep for 2 sec
+    await new Promise(r => setTimeout(r, 2000));
+    return links.map(tagData => ({
+        ...tagData,
+        hasError: Math.random() < 0.3,
+        isValid: Math.random() < 0.5
+    }));
+}
+
+function cleanLink({ tag, href }) {
+    return { tag, href: href && cleanLink(href) };
+}
+
+function styleTag({ tag, hasError, isValid }) {
+    tag.style.color = hasError ? 'gray' : isValid ? 'green' : 'red';
+    tag.style.cursor = hasError ? 'help' : isValid ? 'pointer' : 'not-allowed';
+    console.log(tag.style.color);
+}
+
+async function main() {
+    const links = tags('a')
+        .map(tag => ({
+            tag: tag,
+            href: tag.getAttribute('href')
+        }))
+        .map(cleanLink);
+    links.forEach(({ tag }) => {
+        tag.style.color = 'gray';
+        tag.style.cursor = 'wait';
+    });
+    const validatedLinks = await validateURLS(links);
+    validatedLinks.forEach(styleTag);
+}
+
+main();
